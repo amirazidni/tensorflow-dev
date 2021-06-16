ee7df6db1d4d1f36b9c4a2c7bfe8b4d34d608e0c
Zidni Imani <zidni.imani@gmail.com>
2021-06-16 13:54:45 +0700

    Update Problem_B5.py

--- Submission B/Problem_B5.py
+++ Submission B/Problem_B5.py
@@ -17,6 +17,11 @@ import matplotlib.pyplot as plt
 import csv
 import urllib
 
+class MAE02(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+       if logs.get('mae') < 0.19:
+          print("\nTraining selesai, mencapai mae < 0.2")
+          self.model.stop_training = True
 
 def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
     series = tf.expand_dims(series, axis=-1)
@@ -40,42 +45,60 @@ def solution_B5():
         next(reader)
         step = 0
         for row in reader:
-            temps.append(  # YOUR CODE HERE)
-                time_step.append(  # YOUR CODE HERE)
-                    step=step + 1
+            temps.append(float(row[1]))
+            time_step.append(step)
+            step = step + 1
 
-            series =  # YOUR CODE HERE
+    series = np.array(temps)
 
-            # Normalization Function. DO NOT CHANGE THIS CODE
-            min = np.min(series)
-            max = np.max(series)
-            series -= min
-            series /= max
-            time = np.array(time_step)
+    # Normalization Function. DO NOT CHANGE THIS CODE
+    min = np.min(series)
+    max = np.max(series)
+    series -= min
+    series /= max
+    time = np.array(time_step)
 
-            # DO NOT CHANGE THIS CODE
-            split_time = 2500
+    # DO NOT CHANGE THIS CODE
+    split_time = 2500
 
-            time_train =  # YOUR CODE HERE
-            x_train =  # YOUR CODE HERE
-            time_valid =  # YOUR CODE HERE
-            x_valid =  # YOUR CODE HERE
+    time_train = time[:split_time]
+    x_train = series[:split_time]
+    time_valid = time[split_time:]
+    x_valid = series[split_time:]
 
-            # DO NOT CHANGE THIS CODE
-            window_size = 64
-            batch_size = 256
-            shuffle_buffer_size = 1000
+    # DO NOT CHANGE THIS CODE
+    window_size = 64
+    batch_size = 256
+    shuffle_buffer_size = 1000
 
-            train_set = windowed_dataset(x_train, window_size, batch_size, shuffle_buffer_size)
-            print(train_set)
-            print(x_train.shape)
+    train_set = windowed_dataset(x_train, window_size, batch_size, shuffle_buffer_size)
+    print(train_set)
+    print(x_train.shape)
 
-            model = tf.keras.models.Sequential([
-                # YOUR CODE HERE.
-                tf.keras.layers.Dense(1),
-            ])
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv1D(filters=60, kernel_size=5,
+                               strides=1, padding="causal",
+                               activation="relu",
+                               input_shape=[None, 1]),
+        tf.keras.layers.LSTM(60, return_sequences=True),
+        tf.keras.layers.LSTM(60, return_sequences=True),
+        tf.keras.layers.Dense(30, input_shape=[window_size], activation="relu"),
+        tf.keras.layers.Dense(10, activation="relu"),
+        tf.keras.layers.Dense(1)
+    ])
 
-            # YOUR CODE HERE
+    model.compile(
+        loss=tf.keras.losses.Huber(),
+        optimizer=tf.keras.optimizers.SGD(learning_rate=1e-5, momentum=0.9),
+        metrics=["mae"]
+    )
+    callbacks5 = tf.keras.callbacks.LearningRateScheduler(lambda epoch: 1e-8 * 10 ** (epoch / 20))
+
+    model.fit(train_set,
+              epochs=150,
+              verbose=0,
+              callbacks=[MAE02(), callbacks5]
+              )
     return model
 
 
