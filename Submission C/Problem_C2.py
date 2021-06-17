e5e4f9b7c9155b6f269b673ae3602ab839aeb6f4
Zidni Imani <zidni.imani@gmail.com>
2021-06-17 11:13:12 +0700

    Update Problem_C2.py

--- Submission C/Problem_C2.py
+++ Submission C/Problem_C2.py
@@ -11,11 +11,36 @@
 
 import tensorflow as tf
 
+class Callback2(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if logs.get('accuracy') > 0.97 and logs.get('val_accuracy') > 0.97:
+      print("\nTraining selesai, akurasi tercapai")
+      self.model.stop_training = True
 
 def solution_C2():
     mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-    # YOUR CODE HERE
+    x_train = x_train / 255.0
+    x_test = x_test / 255.0
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    model.fit(
+        x_train,
+        y_train,
+        validation_data=(x_test, y_test),
+        epochs=10,
+        callbacks=[Callback2()],
+        verbose=2
+    )
 
     return model
 
